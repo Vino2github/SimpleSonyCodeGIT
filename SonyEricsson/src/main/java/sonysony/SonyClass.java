4e9842489bf43440fd6caf98bf5e9a597983779b
Vino2github <113656488+Vino2github@users.noreply.github.com>
2022-10-26 12:16:48 +0530

    Add files via upload
    committed 26-10

---
+++ SonyEricsson/src/main/java/sonysony/SonyClass.java
@@ -0,0 +1,11 @@
+package sonysony;
+
+public class SonyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("Siny Sony Ercission");
+	}
+
+}
