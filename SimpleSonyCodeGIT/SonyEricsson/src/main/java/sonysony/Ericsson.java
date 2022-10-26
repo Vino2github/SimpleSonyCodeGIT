e0a5b9a1bea84c7893a4ed092c6aafe5c26eb5a6
Vino2github <113656488+Vino2github@users.noreply.github.com>
2022-10-26 12:44:19 +0530

    Add files via upload
    twoclasses - Sony Ericsson

---
+++ SimpleSonyCodeGIT/SonyEricsson/src/main/java/sonysony/Ericsson.java
@@ -0,0 +1,18 @@
+package sonysony;
+
+public class Ericsson extends SonyClass {
+
+	public static void main(String[] args) {
+		Ericsson obj = new Ericsson();
+		 obj.method2();
+		 obj.methods();
+		 
+	}
+	
+	public  void method2()
+	{
+		System.out.println("Ercisson class method");
+		method();
+		
+	}
+}
---
+++ SimpleSonyCodeGIT/SonyEricsson/src/main/java/sonysony/SonyClass.java
@@ -0,0 +1,31 @@
+package sonysony;
+
+public class SonyClass {
+
+	static int a=1000;
+	static int swing2 = 1200;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SonyClass obj = new SonyClass();
+		System.out.println("Siny Sony Ercission helloooo");
+		method();
+		obj.methods();
+
+	}
+	
+	public static void method()
+	{
+		
+		System.out.println("Swing:  "+ swing2);
+	}
+	
+	public void methods()
+	{
+		
+		System.out.println("methods O/P - Swing1:  "+ swing2);
+		
+	}
+	
+	
+
+}
