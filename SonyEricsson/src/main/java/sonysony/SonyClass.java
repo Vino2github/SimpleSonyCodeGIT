e4a3f401a93e58d00f1496107a9b05a9e53d5af3
Vino2github <113656488+Vino2github@users.noreply.github.com>
2022-10-26 12:17:26 +0530

    Update SonyClass.java
    commited line vino GIT

--- SonyEricsson/src/main/java/sonysony/SonyClass.java
+++ SonyEricsson/src/main/java/sonysony/SonyClass.java
@@ -5,7 +5,7 @@ public class SonyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   
-		System.out.println("Siny Sony Ercission");
+		System.out.println("Siny Sony Ercission/n");
 	}
 
 }
